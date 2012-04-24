ee67c378ac837bd03a3ccce86c80090a8c498ece
Piotr Żurkowski <kareth92@gmail.com>
2012-04-24 18:39:11 +0200

    Add basic echoprint and codegen functionality support

--- script.sh
+++ script.sh
@@ -7,11 +7,18 @@ print_files(){
   do
     if [ -d "$file" ]
     then
-      echo "katalog: $file"
+      #echo "katalog: $file"
       print_files "$file"
     else
-      echo "plik: $file"
+      #echo "plik: $file"
       count=$(( $count + 1 ))
+      ./echoprint-codegen "$file" 50 50 > tmp1
+      grep -Po '"code":".*?[^\\]"' tmp1 > tmp2
+      awk '{print substr($0,9)}' tmp2 > tmp1
+      awk '{print substr($0, 0, length($0) - 1)}' tmp1 > tmp2
+      code=`cat tmp2`
+      #echo "$code"
+      curl "http://developer.echonest.com/api/v4/song/identify?api_key=N6E4NIOVYMTHNDM8J&code=$code"
     fi
   done
 }
